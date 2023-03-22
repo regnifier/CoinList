f17c85f8a7e988b66d5d124b3614e11dbfab86cb
artsiom <artsiommwork@gmail.com>
2023-03-22 16:57:57 +0300

    another another fixes

--- app/src/main/java/com/example/cryptocoinlist/presentation/screens/MainScreen.kt
+++ app/src/main/java/com/example/cryptocoinlist/presentation/screens/MainScreen.kt
@@ -3,7 +3,6 @@ package com.example.cryptocoinlist.presentation.screens
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.example.cryptocoinlist.presentation.activity.MainViewModel
 import com.example.cryptocoinlist.presentation.activity.UiState
